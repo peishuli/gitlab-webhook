c31712f6a201be28981a39fe6a8dfc3717905939
Li Peishu <peishuli62@gmail.com>
2019-09-15 12:57:32 -0500

    fixed a typo in Steps.Command template

--- tekton/utils.go
+++ tekton/utils.go
@@ -6,6 +6,7 @@ import (
 	k8s "k8s.io/client-go/kubernetes"
 	api "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1" 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type BuildInfo struct {
@@ -19,7 +20,12 @@ type Client struct {
 }
 
 func (c Client) CreateTaskRun(buildInfo BuildInfo) {
+	// Create the build task if not exists (an idempontent opration)
+	c.createBuildTask(buildInfo)
+	
+	// Now create taskrun
 	taskrundef := createTaskRunDef(buildInfo)
+
 	_, err := c.TektonClient.TaskRuns("default").Create(taskrundef)
 
 	if err != nil {
@@ -68,11 +74,110 @@ func createTaskRunDef(buildInfo BuildInfo) *api.TaskRun {
 						},
 					},
 				},
-			},
+			},		
 		},
 	}
 
 	return &taskrun
 }
 
+func (c Client) createBuildTask(buildInfo BuildInfo) {
+	taskName := fmt.Sprintf("%s-build-task", buildInfo.ProjectName)
+	_, err := c.TektonClient.Tasks("default").Get(taskName, metav1.GetOptions{})
+
+	if err == nil  {
+		// named task already exists
+		return
+	} 
+	
+	taskDef := createBuildTaskDef(buildInfo)
+
+	_, err = c.TektonClient.Tasks("default").Create(taskDef)
+
+	if err != nil {
+		fmt.Printf("error creating task: %v", err)
+	}
+
+	
+}
+
+func createBuildTaskDef(buildInfo BuildInfo) *api.Task {
+
+	task := api.Task {
+		ObjectMeta: metav1.ObjectMeta {
+			Name: fmt.Sprintf("%s-build-task", buildInfo.ProjectName),
+			Namespace: "default",
+		},
+		Spec: api.TaskSpec {
+			Inputs: &api.Inputs {
+				Resources: []api.TaskResource {
+					api.TaskResource {
+						Name: "source",
+						Type: api.PipelineResourceTypeGit,
+					},
+				},
+				Params: []api.ParamSpec {
+					api.ParamSpec {
+						Name: "DOCKERFILE",
+						Description: "The name of the Dockerfile",
+						Default: &api.ArrayOrString {
+							Type: api.ParamTypeString,
+							StringVal: "Dockerfile",
+						},
+					},
+					api.ParamSpec {
+						Name: "BUILDKIT_CLIENT_IMAGE",
+						Description: "The name of the BuildKit client (buildctl) image",
+						Default: &api.ArrayOrString {
+							Type: api.ParamTypeString,
+							StringVal: "moby/buildkit:v0.5.1@sha256:d45d15f3b22fcfc1b112ffafc40fd2f2d530245e63cfe346a65bd75acdc4d346",
+						},
+					},
+					api.ParamSpec {
+						Name: "BUILDKIT_DAEMON_ADDRESS",
+						Description: "The address of the BuildKit daemon (buildkitd) service",
+						Default: &api.ArrayOrString {
+							Type: api.ParamTypeString,
+							StringVal: "tcp://buildkitd:1234",
+						},
+					},
+					api.ParamSpec {
+						Name: "COMMITID",
+						Description: "Gitlab repo commit Id",
+					},
+				},
+			},
+			Outputs: &api.Outputs {
+				Resources: []api.TaskResource {
+					api.TaskResource {
+						Name: "image",
+						Type: api.PipelineResourceTypeImage,
+					},
+				},
+			},
+			Steps: []api.Step {
+				api.Step {
+					//Name: "something",
+					corev1.Container {
+						Name: "build-and-push",
+						Image: "${inputs.params.BUILDKIT_CLIENT_IMAGE}",
+						WorkingDir: "/workspace/source",
+						Command: []string {
+							"buildctl", "--debug", "--addr=${inputs.params.BUILDKIT_DAEMON_ADDRESS}", "build",
+							"--progress=plain",
+							"--frontend=dockerfile.v0",
+							"--opt", "filename=${inputs.params.DOCKERFILE}",
+							"--local", "context=.", "--local", "dockerfile=.",
+							"--output", "type=image,name=${outputs.resources.image.url}:dev-$(inputs.params.COMMITID),push=true",
+							"--export-cache", "type=inline",
+							"--import-cache", "type=registry,ref=${outputs.resources.image.url}",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return &task
+}
 
